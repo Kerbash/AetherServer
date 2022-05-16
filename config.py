561a4f954b1a602ed846a9e412e902509b6ce28d
William Selke <74438653+Kerbash@users.noreply.github.com>
2022-05-16 09:21:06 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ from pymongo.server_api import ServerApi
 class Config:
     VERSION = "A 1.00"
     DRIVE = "G"
-    DATABASE = pymongo.MongoClient("mongodb+srv://guyokay:12345@aetherdrive.d6b4m.mongodb.net/Sensor?retryWrites=true&w=majority")
+    
+    // TODO Change the user and password to the match yours
+    DATABASE = pymongo.MongoClient("mongodb+srv://USER:PASSWORD@aetherdrive.d6b4m.mongodb.net/Sensor?retryWrites=true&w=majority")
